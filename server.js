123600ee34b7e7f7a1517bb6b21211bf1eafeac6
PamelaCleveland <pamelac021@gmail.com>
2021-10-17 18:00:33 -0400

    reverse changes server

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
-app.use('/api', apiRoutes);
-app.use('*', htmlRoutes);
+require('./routes/apiRoutes.js')(app);
+require('./routes/htmlRoutes.js')(app);
 
 
 app.listen(PORT, function() {
