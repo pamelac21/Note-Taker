037ebc5c1cdfcd34ccbbcffde6a27ec45fa383f4
PamelaCleveland <pamelac021@gmail.com>
2021-10-16 21:34:25 -0400

    Add Heroku

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,33 +1,49 @@
 const fs = require("fs");
 const path = require("path");
+const noteArray = require("../db/db.json");
 //unique id npm package
-const {v4 : uuidv4} = require('uuidv4')
+const { uuid } = require("uuidv4");
 
 module.exports = (app) => {
-    app.get("/api/notes", (req, res) => {
-        res.json(notesArray);
-    })
-    app.post("/api/notes", (req, res) => {
+  app.get("/api/notes", (req, res) => {
+    res.json(noteArray);
+  });
 
-        
-        const newNote = {
-            id: uuid(),
-            title: req.body.title,
-            text: req.body.text
-        }
+  app.post("/api/notes", (req, res) => {
+    const newNote = {
+      id: uuid(),
+      title: req.body.title,
+      text: req.body.text,
+    };
 
-        console.log(newNote);
-        const file = path.join(__dirname, "../db/db.json");
+    console.log(newNote);
+    const file = path.join(__dirname, "../db/db.json");
 
-        notesArray.push(newNote);
-        
-        fs.writeFile(file, JSON.stringify(notesArray, null, 4), err => {
-            if (err) throw err;
-            console.log("New note has been saved!");
-        });
+    noteArray.push(newNote);
 
-        res.send(newNote);
+    fs.writeFile(file, JSON.stringify(noteArray, null, 4), (err) => {
+      if (err) throw err;
+      console.log("New note has been saved!");
     });
 
-    
-}
+    res.send(newNote);
+  });
+
+  app.delete("/api/notes/:id", (req, res) => {
+    res.send("DELETE Request Called");
+    const id = req.params.id;
+    const file = path.join(__dirname, "../db/db.json");
+
+    for (const note of noteArray) {
+      if (id === note.id) {
+        const index = noteArray.indexOf(note);
+        noteArray.splice(index, 1);
+        fs.writeFile(file, JSON.stringify(noteArray, null, 4), (err) => {
+          if (err) throw err;
+          console.log("Note deleted!");
+        });
+        res.end();
+      }
+    }
+  });
+};
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 //const apiRoutes = require("./routes/apiRoutes.js");
 //const htmlRoutes = require("./routes/htmlRoutes.js");
 const PORT = process.env.PORT || 3000;
-const uuidv4 = require('uuidv4')
+const uuidv4 = require('uuidv4');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
